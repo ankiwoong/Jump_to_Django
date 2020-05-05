439753da90b130a8c53d24022b83ab76ed4cde31
AnKiWoong <ankiwoong@gmail.com>
2020-05-05 19:11:03 +0900

    Django admin fixes

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question
+
+
+# Question 모델을 관리자에 등록
+# 관리자 화면에서 제목으로 질문을 검색할 수 있는 기능을 추가
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+admin.site.register(Question, QuestionAdmin)
