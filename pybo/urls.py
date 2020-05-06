68c00a51c377b28e90fc963c6a32a65a86f940b5
ankiwoong <ankiwoong@gmail.com>
2020-05-06 12:27:54 +0900

    Data storage

--- pybo/urls.py
+++ pybo/urls.py
@@ -8,4 +8,7 @@ app_name = 'pybo'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
+    # http://locahost:8000/pybo/answer/create/2/ 와 같은 페이지를 요청하면 views.answer_create 함수를 호출
+    path('answer/create/<int:question_id>/',
+         views.answer_create, name='answer_create'),
 ]
