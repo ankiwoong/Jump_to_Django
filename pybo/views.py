22add68e9cd31660298de9dae83919492132401f
ankiwoong <ankiwoong@gmail.com>
2020-05-06 16:14:07 +0900

    Debugging

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Question
 from .forms import QuestionForm, AnswerForm
@@ -10,8 +9,7 @@ def index(request):
     """
     pybo 목록 출력
     """
-    question_list = Question.objects.order_by(
-        '-create_date')       # 작성일시의 역순으로 정렬
+    question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
@@ -20,7 +18,6 @@ def detail(request, question_id):
     """
     pybo 내용 출력
     """
-    # question = Question.objects.get(id=question_id)
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
