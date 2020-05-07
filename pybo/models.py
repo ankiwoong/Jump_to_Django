968283bdf5bafa42563f001f38585c2648f577db
ankiwoong <ankiwoong@gmail.com>
2020-05-07 10:10:57 +0900

    Change Model

--- pybo/models.py
+++ pybo/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
     '''
+    author      글쓴이
     subject	    질문의 제목
     content 	질문의 내용
     create_date	질문을 작성한 일시
     '''
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
@@ -17,10 +20,12 @@ class Question(models.Model):
 
 class Answer(models.Model):
     '''
+    author      글쓴이
     question    질문 (어떤 질문의 답변인지 알아야하므로 질문 속성이 필요하다.)
     content     답변의 내용
     create_date	답변을 작성한 일시
     '''
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
-
 from .models import Question
 from .forms import QuestionForm, AnswerForm
 
@@ -33,6 +33,7 @@ def detail(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     """
     pybo 답변등록
@@ -42,6 +43,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # 추가한 속성 author 적용
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -52,6 +54,7 @@ def answer_create(request, question_id):
     return render(request, 'pybo/question_detail.html', context)
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     """
     pybo 질문등록
@@ -60,6 +63,7 @@ def question_create(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # 추가한 속성 author 적용
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
