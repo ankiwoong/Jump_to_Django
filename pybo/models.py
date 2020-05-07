98641a201b5b02cb3b9df666ce5d3c171ed1ba33
ankiwoong <ankiwoong@gmail.com>
2020-05-07 10:57:32 +0900

    Edit and Delete

--- pybo/models.py
+++ pybo/models.py
@@ -8,11 +8,13 @@ class Question(models.Model):
     subject	    질문의 제목
     content 	질문의 내용
     create_date	질문을 작성한 일시
+    modify_date 질문을 수정한 일시
     '''
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -24,8 +26,10 @@ class Answer(models.Model):
     question    질문 (어떤 질문의 답변인지 알아야하므로 질문 속성이 필요하다.)
     content     답변의 내용
     create_date	답변을 작성한 일시
+    modify_date 답변을 수정한 일시
     '''
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
